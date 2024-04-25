f9ec1daba5ee31940a5e16bb7f0b92445d7d3acb
Leo Borcherding <leoleojames1@gmail.com>
2024-04-25 17:03:23 -0500

    Speech to Speech Update 0.2 alpha

---
+++ ollama_mod_cage/tts_processor.py
@@ -0,0 +1,56 @@
+# tts_processor.py
+
+import sounddevice as sd
+import os
+import torch
+import re
+from TTS.api import TTS
+
+class tts_processor:
+    def __init__(self):
+        self.current_dir = os.getcwd()
+        self.parent_dir = os.path.abspath(os.path.join(self.current_dir, os.pardir))
+
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.tts_wav_path = os.path.join(self.parent_dir, "AgentFiles\\Ignored_TTS\\pipeline\\active_group\\clone_speech.wav")
+        self.tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(self.device)
+
+    def process_tts_responses(self, response):
+        # Call Sentence Splitter
+        tts_response_sentences = self.split_into_sentences(response)
+        self.generate_play_audio_loop(tts_response_sentences)
+        return
+
+    def generate_play_audio_loop(self, tts_response_sentences):
+        # Generate audio for each sentence in TTS
+        flag = True
+        for sentence in tts_response_sentences:
+            # If not first wave wait again
+            if flag == False:
+                sd.wait()
+            # Generate wav file
+            tts_audio = self.tts.tts(text=sentence, speaker_wav=(f"{self.tts_wav_path}"), language="en")
+            # Play Audio
+            sd.play(tts_audio, samplerate=22050)
+            # If not first wav file wait
+            if flag == False:
+                sd.wait()
+            flag = False
+        return
+
+    def split_into_sentences(self, text: str) -> list[str]:
+        # Add spaces around punctuation marks for consistent splitting
+        text = " " + text + " "
+        text = text.replace("\n", " ")
+
+        # Handle common abbreviations and special cases
+        text = re.sub(r"(Mr|Mrs|Ms|Dr|i\.e)\.", r"\1<prd>", text)
+        text = re.sub(r"\.\.\.", r"<prd><prd><prd>", text)
+
+        # Split on period, question mark, or exclamation mark followed by optional spaces
+        sentences = re.split(r"[.!?]\s*", text)
+
+        # Remove empty sentences
+        sentences = [s.strip() for s in sentences if s.strip()]
+
+        return sentences
